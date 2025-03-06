e1ed33f7547a53fe747c6f131408d21c24d960e3
FreddyPinto <freddypinto@outlook.com>
2025-03-05 20:36:43 -0500

    Add main file

---
+++ app/main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI, status
+from fastapi.middleware.cors import CORSMiddleware
+from motor.motor_asyncio import AsyncIOMotorClient
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from collections import defaultdict
+from app.config import BaseConfig
+from app.routers.cars import cars_router
+
+settings = BaseConfig()
+
+
+async def lifespan(app: FastAPI):
+    app.client = AsyncIOMotorClient(settings.DB_URL)
+    app.db = app.client[settings.DB_NAME]
+    try:
+        # Prueba la conexión a MongoDB
+        await app.client.admin.command("ping")
+        print("Pinged your deployment. You have successfully connected to MongoDB!")
+        print("Mongo address:", settings.DB_URL)
+    except Exception as e:
+        print("Failed to ping MongoDB:", e)
+    yield
+    # Cerrar la conexión a MongoDB al terminar
+    app.client.close()
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(cars_router, prefix="/cars", tags=["cars"])
+
+
+@app.get("/")
+async def get_root():
+    return {"Message": "Root working"}
