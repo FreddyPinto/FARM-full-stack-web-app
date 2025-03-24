f9c08fae5eacce14c1dc4c7e5f07d9db67db269b
FreddyPinto <freddypinto@outlook.com>
2025-03-24 14:33:37 -0500

    Add picture upload functionality to car creation endpoint

--- app/routers/cars.py
+++ app/routers/cars.py
@@ -1,22 +1,79 @@
-from turtle import update
+from turtle import update, width
 from bson import ObjectId
-from fastapi import APIRouter, Body, Depends, HTTPException, Request, status
+import cloudinary
+from fastapi import (
+    APIRouter,
+    Body,
+    Depends,
+    HTTPException,
+    Request,
+    UploadFile,
+    status,
+    Form,
+    File,
+)
 from pymongo import ReturnDocument
-from app.models.car_model import CarCollectionPagination, CarModel, CarCollection, UpdateCarModel
-from cloudinary import uploader
+from app.models.car_model import (
+    CarCollectionPagination,
+    CarModel,
+    CarCollection,
+    UpdateCarModel,
+)
+from app.config import BaseConfig
 
+settings = BaseConfig()
 cars_router = APIRouter()
 
 CARS_PER_PAGE = 10
 
+cloudinary.config(
+    cloud_name=settings.CLOUDINARY_CLOUD_NAME,
+    api_key=settings.CLOUDINARY_API_KEY,
+    api_secret=settings.CLOUDINARY_SECRET_KEY,
+)
+
+
+# @cars_router.post(
+#     "/",
+#     response_description="Add new car",
+#     response_model=CarModel,
+#     status_code=status.HTTP_201_CREATED,
+#     response_model_by_alias=False,
+# )
+# async def add_car(request: Request, car: CarModel = Body(...)):
+#     cars = request.app.db["cars"]
+#     document = car.model_dump(by_alias=True, exclude=["id"])
+#     inserted = await cars.insert_one(document)
+#     return await cars.find_one({"_id": inserted.inserted_id})
+
+
 @cars_router.post(
     "/",
-    response_description="Add new car",
+    response_description="Add new car with picture",
     response_model=CarModel,
     status_code=status.HTTP_201_CREATED,
-    response_model_by_alias=False,
 )
-async def add_car(request: Request, car: CarModel = Body(...)):
+async def add_car_with_picture(
+    request: Request,
+    brand: str = Form("brand"),
+    make: str = Form("make"),
+    year: int = Form("year"),
+    cm3: int = Form("cm3"),
+    km: int = Form("km"),
+    price: int = Form("price"),
+    picture: UploadFile = File("picture"),
+):
+    cloudinary_image = cloudinary.uploader.upload(picture.file, crop="fill", width=800)
+    picture_url = cloudinary_image["url"]
+    car = CarModel(
+        brand=brand,
+        make=make,
+        year=year,
+        cm3=cm3,
+        km=km,
+        price=price,
+        picture_url=picture_url,
+    )
     cars = request.app.db["cars"]
     document = car.model_dump(by_alias=True, exclude=["id"])
     inserted = await cars.insert_one(document)
