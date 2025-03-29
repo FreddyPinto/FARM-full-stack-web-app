2eec02f3eacc7c9c5b23b2b0354c9ca4acc831f0
FreddyPinto <freddypinto@outlook.com>
2025-03-29 15:30:25 -0500

    Add login endpoint and refactor user registration response handling

--- app/routers/users.py
+++ app/routers/users.py
@@ -9,9 +9,7 @@ auth_handler = AuthHandler()
 
 
 @users_router.post(
-    "/register",
-    response_description="Register user",
-    response_model=UserModel
+    "/register", response_description="Register user", response_model=UserModel
 )
 async def register(request: Request, newUser: LoginModel = Body(...)) -> UserModel:
     users = request.app.db["users"]
@@ -29,5 +27,17 @@ async def register(request: Request, newUser: LoginModel = Body(...)) -> UserMod
         )
     new_user = await users.insert_one(newUser)
     created_user = await users.find_one({"_id": new_user.inserted_id})
-
     return created_user
+
+
+@users_router.post("/login", response_description="Login user")
+async def login(request: Request, loginUser: LoginModel = Body(...)) -> JSONResponse:
+    users = request.app.db["users"]
+    user = await users.find_one({"username": loginUser.username})
+    if (user is None) or (
+        not auth_handler.verify_password(loginUser.password, user["password"])
+    ):
+        raise HTTPException(status_code=401, detail="Invalid username and/or password")
+    token = auth_handler.encode_token(str(user["_id"]), user["username"])
+    response = JSONResponse(content={"token": token, "username": user["username"]})
+    return response
