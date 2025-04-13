828e16aaff78ed842e1ae541d54bf29aa1313ec6
FreddyPinto <freddypinto@outlook.com>
2025-04-13 12:54:40 -0500

    Add AuthRequired component to enforce authentication for protected routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,13 +13,16 @@ import NewCar from "./pages/NewCar";
 import SingleCar from "./pages/SingleCar";
 import NotFound from "./pages/NotFound";
 import { AuthProvider } from "./contexts/AuthProvider";
+import AuthRequired from "./components/AuthRequired";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
       <Route index element={<Home />} />
       <Route path="/cars" element={<Cars />} loader={carsLoader} />
-      <Route path="/new-car" element={<NewCar />} />
+      <Route element={<AuthRequired />}>
+        <Route path="/new-car" element={<NewCar />} />
+      </Route>
       <Route path="/cars/:id" element={<SingleCar />} />
       <Route path="/login" element={<Login />} />
       <Route path="*" element={<NotFound />} />
